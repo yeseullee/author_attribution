2c7429057afbdd260de18ce18fa0186e9dd5eb5a
Yeseul Lee (Galileo) <yeseullee@users.noreply.github.com>
2016-11-10 00:31:36 -0500

    updated

--- bow.py
+++ bow.py
@@ -30,17 +30,26 @@ def stringToPath(docname):
     return path + "/" + docname + ".txt"
 
 #make_train_test_docs(singleList, 50000)
-def make_train_test_docs(singleList, docnum):
+def make_train_test_docs(singleMap, docnum):
+
+    #This is for changing (author,title):docname to docname:author pairs
+    #The previous version was for removing duplicates. Now we need the latter.
+    singleList = dict()
+    for k in singleMap.keys():
+        singleList[singleMap[k]] = k[0]
+
     docnames = singleList.keys()
     numTrainD = 0
     numTestD = 0
     numDocs = 0
-    train_doc_authors = set()
+    train_doc_authors = []
 
+    authornames = file('author_names.txt', 'w')
     trainf = file('train_docs.txt', 'w')
     train_auth = file('train_authors.txt', 'w')
     testf = file('test_docs.txt', 'w')
     test_auth = file('test_authors.txt', 'w')
+
     for name in docnames:
         path = "/scratch4/yeseul/docs/txt" + stringToPath(name)
         print path
@@ -49,19 +58,20 @@ def make_train_test_docs(singleList, docnum):
             author = singleList[name]
             if author in train_doc_authors:
                 testf.write(name+";")
-                test_auth.write(author+";")
+                test_auth.write(str(train_doc_authors.index(author))+";")
                 numTestD = numTestD + 1
             else:
-                train_doc_authors.add(author)
+                train_doc_authors.append(author)
+                authornames.write(author+";")
                 trainf.write(name+";")
-                train_auth.write(author+";")
+                train_auth.write(str(train_doc_authors.index(author))+";")
                 numTrainD = numTrainD + 1
 
             numDocs = numDocs +1
-
             print str(numDocs)
         if numDocs >= docnum:
             break
+    authornames.close()
     trainf.close()
     testf.close()
     train_auth.close()
@@ -86,12 +96,13 @@ def cleanfiles(filelist, outputfilename):
 
 def bow():
     
-    #Read in target names
+    #Read in train authors
     targetf = file('train_authors.txt','r')
     targetnames = targetf.read()
     targetf.close()
     targetnames = targetnames.split(';')
-    targets = range(0,len(targetnames)-1)
+    targetnames = targetnames[:-1]
+    targetnames = map(int, targetnames)
 
     #Read in cleaned train texts
     trainf = file('cleaned_train_docs.txt', 'r')
@@ -103,10 +114,10 @@ def bow():
     #Creating features from a bag of words
     print "Now running count vectorizer"        
     #Initialize the CountVectorizer object.
-    vectorizer = CountVectorizer(analyzer="word", tokenizer= None, preprocessor = None, stop_words = None, max_features =5000)
+    vectorizer = CountVectorizer(analyzer="word", tokenizer= None, preprocessor = None, stop_words = None, max_features =20000)
 
     #fit_transform() does 1. fit the model and learn the vocab. 2. transform the data into feature vectors. Input to fit_transform should be a list of strings.
-    #Basically 5000 vocabs and number of txt files. How many each vocab in each txt file.
+    #Basically 20000 vocabs and number of txt files. How many each vocab in each txt file.
 
     train_data_features = vectorizer.fit_transform(cleaned_txt)
     #to convert to a np array.
@@ -121,7 +132,7 @@ def bow():
     print "Now training classifier"
     #Then train a classifier.
     #Use Multinomail variant Naive Bayes classifier.
-    classifier = MultinomialNB().fit(train_tfid, targets)
+    classifier = MultinomialNB().fit(train_tfid, targetnames)
     
     #Try to predict the outcome.
     #Get test_docs
@@ -136,25 +147,44 @@ def bow():
     test_tfid = tf_transformer.transform(test_counts)
     
     predicted = classifier.predict(test_tfid)
-    '''
+    
+    print "Writing predictions"
     #Output file for predictions.
     predictf = file('prediction.txt', 'w')
     for p in predicted:
-        predictf.write(targetnames[p]+';')
+        predictf.write(str(targetnames[p])+';')
     predictf.close()
-    '''
 
+    #Get test authors to check for answers
+    f = file('test_authors.txt', 'r')
+    test_authors = f.read()
+    f.close()
+    test_authors = test_authors.split(';')
+    test_authors = test_authors[:-1]
+    test_authors = map(int, test_authors)
+    
+    #Read author names
+    f = file('author_names.txt','r')
+    authornames = f.read()
+    f.close()
+    authornames = authornames.split(';')
+    authornames = authornames[:-1]
+ 
+    print metrics.classification_report(test_authors, predicted, target_names=authornames) 
+
+def main():
     '''
-    #TODO: make target numbers rather than string.
-    test_targetf = file("test_authors.txt", "r")
-    test_target = f.read()
-    test_targetf.close()
-    test_target = test_target.split(';')
-    
-    test_target_index = []
-    for i in test_target:
-        if i in targetnames:
-            test_target_index.append(targetnames.index(i))
-    
-   # print metrics.classification_report(test_target_index, predicted, target_names=targetnames) 
-   '''
+    f = file('train_docs.txt', 'r')
+    r = f.read()
+    f.close()
+    trainlist = r.split(';')
+    cleanfiles(trainlist, 'cleaned_train_docs.txt')
+
+    f = file('test_docs.txt', 'r')
+    r = f.read()
+    f.close()
+    testlist = r.split(';')
+    cleanfiles(testlist, 'cleaned_test_docs.txt')
+    '''
+    bow()
+main()
